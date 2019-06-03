7176b5921b8d0755098bb0365943a0cd1c272079
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-06-03 12:28:19 +0100

    improve readability

--- opentargets_validator/helpers.py
+++ opentargets_validator/helpers.py
@@ -14,9 +14,6 @@ import functools
 from contextlib import contextmanager
 import opentargets_validator
 
-_l = logging.getLogger(__name__)
-
-
 class URLZSource(object):
     def __init__(self, filename, *args, **kwargs):
         """A source extension for petl python package
@@ -116,43 +113,6 @@ def generate_validator_from_schema(schema_uri):
         )
 
     return validator
-
-
-class LogAccum(object):
-    def __init__(self, logger_o, elem_limit=1024):
-        self._logger = logger_o
-        self._accum = {'counter': 0}
-        self._limit = elem_limit
-
-    def _flush(self, force=False):
-        flushed = False
-        if force or self._accum['counter'] >= self._limit:
-            keys = set(self._accum.keys()) - set(['counter'])
-
-            for k in keys:
-                for msg in self._accum[k]:
-                    self._logger.log(k, msg[0], *msg[1])
-            # reset the accum
-            self._accum = {'counter': 0}
-            flushed = True
-        
-        return flushed
-
-    def flush(self, force=True):
-        return self._flush(force)
-
-    def log(self, level, message, *args):
-        if level in self._accum:
-            self._accum[level].append((message, args))
-        else:
-            self._accum[level] = [(message, args)]
-
-        self._accum['counter'] += 1
-        self._flush()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.flush(True)
-
 
 def file_or_resource(fname=None):
     '''get filename and check if in getcwd then get from
--- opentargets_validator/validator.py
+++ opentargets_validator/validator.py
@@ -6,44 +6,31 @@ import logging
 import json
 import codecs
 from sys import exit
-from .helpers import generate_validator_from_schema, LogAccum
+from .helpers import generate_validator_from_schema
 
 
 def validate(file_descriptor, schema_uri, loglines):
     logger = logging.getLogger(__name__)
     error_lines = abs(loglines)
-    l = LogAccum(logger,error_lines if error_lines < 1024 else 1024)
     line_counter = 1
     parsed_line = None
     valid = True
 
     validator = generate_validator_from_schema(schema_uri)
 
-    #for line in codecs.EncodedFile(file_descriptor, "UTF-8"):
     for line in file_descriptor:
         try:
             parsed_line = json.loads(line)
         except Exception as e:
-            l.log(logging.ERROR, 'failed parsing line %i: %s', line_counter, e)
+            logger.error('failed parsing line %i: %s', line_counter, e)
 
-        t1 = time.time()
-        validation_errors = [str(e) for e in validator.iter_errors(parsed_line)]
-        t2 = time.time()
+        validation_errors = [error for error in validator.iter_errors(parsed_line)]
 
         if validation_errors:
             valid = False
-            # here I have to log all fails to logger and elastic
-            error_messages = ' '.join(validation_errors).replace('\n', ' ; ').replace('\r', '')
-
-            error_messages_len = len(error_messages)
-
-            # capping error message to 2048
-            error_messages = error_messages if error_messages_len <= 2048 \
-                else error_messages[:2048] + ' ; ...'
-
-            l.log(logging.ERROR, 'failed validating line %i '
-                  'eval %s secs with these errors %s',
-                  line_counter, str(t2 - t1), error_messages)
+            for validation_error in validation_errors:
+                logger.error('fail @ %i.%s %s',
+                    line_counter, ".".join(validation_error.absolute_path), validation_error.message)
 
             error_lines -= 1
             if error_lines <= 0:
@@ -51,5 +38,4 @@ def validate(file_descriptor, schema_uri, loglines):
 
         line_counter += 1
 
-    l.flush(True)
     return valid
