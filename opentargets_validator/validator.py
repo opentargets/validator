faf4e99d0438132a74758372c11b7f2d1702eb0d
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-08-15 16:25:59 +0100

    Switch to pathos.multiprocessing for higher performance

--- opentargets_validator/validator.py
+++ opentargets_validator/validator.py
@@ -1,24 +1,14 @@
-import concurrent.futures
 import json
 import logging
-import multiprocessing
-
+import pathos.multiprocessing
 import fastjsonschema
 
 from .helpers import box_text
 
 
-def init_validation_pool(validator, logger):
-    # Initialise pool process global variables.
-    global g_validator
-    global g_logger
-    g_validator = validator
-    g_logger = logger
-
-
-def validate_single_line(line_number, line):
+def validate_single_line(line_number, line, validator, logger):
     def format_error(error_type):
-        g_logger.error(f'Line #{line_number} {error_type}. Error:\n{box_text(str(e))}\n{line}\n\n\n')
+        logger.error(f'Line #{line_number} {error_type}. Error:\n{box_text(str(e))}\n{line}\n\n\n')
 
     # Lines come directly from file object-like iterators, so we should strip the end of line characters.
     line = line.rstrip()
@@ -32,7 +22,7 @@ def validate_single_line(line_number, line):
 
     # Does the JSON object in the line validate against the schema?
     try:
-        g_validator(parsed_line)
+        validator(parsed_line)
     except Exception as e:
         format_error('is a valid JSON object, but it does not match the schema')
         return False
@@ -54,15 +44,14 @@ def validate(data_fd, schema_fd):
     validator = fastjsonschema.compile(schema)
 
     # Validate all input lines concurrently.
-    with concurrent.futures.ProcessPoolExecutor(max_workers=multiprocessing.cpu_count(), initializer=init_validation_pool, initargs=(validator, logger)) as executor:
-        # Submit the calculations.
-        futures = [
-            executor.submit(validate_single_line, line_number, line)
-            for line_number, line in enumerate(data_fd, 1)
-        ]
-        # Process results.
-        validity = list([f.result() for f in concurrent.futures.as_completed(futures)])
-        valid, invalid = len([v for v in validity if v]), len([v for v in validity if not v])
+    with pathos.multiprocessing.ProcessingPool(processes=pathos.multiprocessing.cpu_count()) as pool:
+        validity = pool.map(
+            lambda args: validate_single_line(*args),
+            [(line_number, line, validator, logger)
+             for line_number, line in enumerate(data_fd, 1)]
+        )
 
+    # Process the results.
+    valid, invalid = len([v for v in validity if v]), len([v for v in validity if not v])
     logger.info(f'Processing is completed. Total {valid} valid records, {invalid} invalid records.')
     return invalid == 0
