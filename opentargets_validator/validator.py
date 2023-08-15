ac18446ce4892aacea62367bc5a00994b8b471b9
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-08-15 12:58:05 +0100

    Calculate summary of valid/invalid evidence strings

--- opentargets_validator/validator.py
+++ opentargets_validator/validator.py
@@ -7,10 +7,12 @@ import jsonschema
 import pypeln
 import simplejson as json
 
-from .helpers import generate_validator_from_schema
+from .helpers import generate_validator_from_schema, box_text
 
 
-def validate_single_line(line_number, line, validator, logger):
+def validate_single_line(line_number, line, schema, logger):
+    def format_error(error_type):
+        logger.error(f'Line #{line_number} {error_type}. Error:\n{box_text(str(e))}\nFull line: {line}\n\n\n')
 
     # Lines come directly from file object-like iterators, so we should strip the end of line characters.
     line = line.rstrip()
@@ -19,13 +21,14 @@ def validate_single_line(line_number, line, validator, logger):
     try:
         parsed_line = json.loads(line)
     except Exception as e:
-        logger.error(f'Line #{line_number} is not a valid JSON object. Full line: ~~~{line}~~~. Error: ~~~{e}~~~.')
+        format_error('is not a valid JSON object')
         return False
 
     # Does the JSON object in the line validate against the schema?
-    validation_errors = ' ||| '.join([(".".join(error.absolute_path), error.message) for error in validator.iter_errors(parsed_line)])
-    if validation_errors:
-        logger.error(f'Line #{line_number} is a valid JSON object, but it does not match the schema. Full line: ~~~{line}. Error: ~~~{validation_errors}~~~.')
+    try:
+        jsonschema.validate(instance=parsed_line, schema=schema)
+    except jsonschema.exceptions.ValidationError as e:
+        format_error('is a valid JSON object, but it does not match the schema')
         return False
     return True
 
@@ -52,29 +55,28 @@ def validator_mapped(data, validator, logger):
 def validate(data_fd, schema_fd):
     logger = logging.getLogger(__name__)
 
-    # Create the validator object.
+    # Load the schema and check if it is itself valid.
     try:
         schema = json.load(schema_fd)
     except Exception as e:
         logger.error('JSON schema is not valid. Error: ~~~{e}~~~.')
         return False
-    validator = jsonschema.Draft7Validator(schema)
 
     # Validate all input lines concurrently.
-    file_is_valid = True
     with concurrent.futures.ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
         # Submit the calculations.
         futures = [
-            executor.submit(validate_single_line, line_number, line, validator, logger)
+            executor.submit(validate_single_line, line_number, line, schema, logger)
             for line_number, line in enumerate(data_fd, 1)
         ]
         # Process results.
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            if not result:
-                file_is_valid = False
+        validity = list([f.result for f in concurrent.futures.as_completed(futures)])
+        valid, invalid = len([v for v in validity if v]), len([v for v in validity if not v])
 
-    return file_is_valid
+    logger.info(f'Processing is completed. Total {valid} valid records, {invalid} invalid records.')
+    return invalid == 0
+
+
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(validate_single_line_partial, validate_iterator)
